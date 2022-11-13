f5007d71f70cf9a0d0d675ea51baa6da94a421d4
ajaythekkiniyi <ajaythekkiniyi@gmail.com>
2022-11-13 14:38:24 +0530

    api urls changed

--- src/components/Constants.js
+++ src/components/Constants.js
@@ -1,8 +1,8 @@
 const Constants = {
     apiEndPoint : 'https://sea-lion-app-fg24b.ondigitalocean.app/api/', //strapi url
-    apiEndPointNodejs : 'http://localhost:3001', //nodejs url
+    apiEndPointNodejs : 'http://128.199.57.67', //nodejs url
     imageUrl : 'https://sea-lion-app-fg24b.ondigitalocean.app', //strapi url
-    paymentStatusUrl : 'http://localhost:3001/payment-status', //nodejs url
+    paymentStatusUrl : 'http://128.199.57.67/payment-status', //nodejs url
 }
 
 export default Constants

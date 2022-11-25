e04ecb05839e5c6f6624dc3ce40a8b086b54b966
ajaythekkiniyil <ajaythekkiniyi@gmail.com>
2022-11-25 14:29:41 +0530

    api end point updated

--- src/components/Constants.js
+++ src/components/Constants.js
@@ -1,5 +1,5 @@
 const Constants = {
-    apiEndPoint : 'https://oyster-app-5aaav.ondigitalocean.app/api/', //strapi url
+    apiEndPoint : 'https://starfish-app-pwcul.ondigitalocean.app/api/', //strapi url
     // apiEndPointNodejs : 'https://testsites.online', //nodejs url
     // paymentStatusUrl : 'https://testsites.online/payment-status', //nodejs url
     // imageUrl : 'http://localhost:1337', //strapi url
